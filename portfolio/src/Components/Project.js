575df1c5425ca5a63ff1ebd61af2d1290ce9c830
Jonatahn Bajada <jbajada@ryerson.ca>
2020-07-13 18:58:24 -0400

    added blog borders

--- portfolio/src/Components/Project.js
+++ portfolio/src/Components/Project.js
@@ -6,13 +6,31 @@ const Project = (props) => {
 	return (
 		<div className="content-body">
 			<article>
+				<img className="proj-img" src={require('../img/visible.jpg')} alt="" />
 				<h2>Visible</h2>
 				<p>
-					Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most
-					popular web browser today!
+					An application that caters towards people who are often marginalized. The application allows users
+					to upload a txt file or a pdf and is given options to change the output. Changes include: font, font
+					size, font weight, font colour, document background colour, underline all hyperlinks and etc. All
+					selected changes will be applied and outputted onto the screen. The user can then save the processed
+					txt file into their computer. Alternatively, user can upload a txt file, and choose between a female
+					or male voice (if unselected the default voice will be female), the text will be automatically read
+					by an artificial voice. The entire application will comply to the Web Content Accessibility
+					Guidelines (WCAG).
 				</p>
-				<a href="http://visible.com.s3-website.ca-central-1.amazonaws.com/" class="button">
-					Click Here
+				<a
+					className="live-button"
+					href="http://visible.com.s3-website.ca-central-1.amazonaws.com/"
+					class="button"
+				>
+					Live
+				</a>
+				<a
+					className="git-button"
+					href="http://visible.com.s3-website.ca-central-1.amazonaws.com/"
+					class="button"
+				>
+					Github
 				</a>
 			</article>
 
